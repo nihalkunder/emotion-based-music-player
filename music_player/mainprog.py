a60a57a01ad25f47667d0cd59837bdcad1e68ede
nihalkunder <109746366+nihalkunder@users.noreply.github.com>
2023-06-03 11:39:35 +0530

    Add files via upload

---
+++ music_player/mainprog.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, session, flash
+import sqlite3 as sql
+import os
+import requests
+import json
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/gohome')
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/enternew')
+def new_user():
+    return render_template('signup.html')
+
+
+@app.route('/addrec', methods=['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        try:
+            nm = request.form['Name']
+            phonno = request.form['MobileNumber']
+            email = request.form['email']
+            unm = request.form['Username']
+            passwd = request.form['password']
+            with sql.connect("emotify.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO emotifyuser(name,phono,email,username,password)VALUES(?, ?, ?, ?,?)",
+                            (nm, phonno, email, unm, passwd))
+                con.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+
+        finally:
+            return render_template("login.html", msg=msg)
+            con.close()
+
+
+@app.route('/userlogin')
+def user_login():
+    return render_template("login.html")
+
+
+@app.route('/logindetails', methods=['POST', 'GET'])
+def logindetails():
+    if request.method == 'POST':
+        usrname = request.form['username']
+        passwd = request.form['password']
+
+        with sql.connect("emotify.db") as con:
+            cur = con.cursor()
+            cur.execute(
+                "SELECT username,password FROM emotifyuser where username=? ", (usrname,))
+            account = cur.fetchall()
+
+            for row in account:
+                database_user = row[0]
+                database_password = row[1]
+                if database_user == usrname and database_password == passwd:
+                    session['logged_in'] = True
+                    return render_template('home.html')
+                else:
+                    flash("Invalid user credentials")
+                    return render_template('login.html')
+
+
+@app.route('/predictinfo')
+def predictin():
+    return render_template('info.html')
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predcrop():
+    if request.method == 'POST':
+
+        os.system('python real_time_video.py')
+
+        # response = 'Completed'
+        # from real_time_video import happy1, status.angry, status.surprised, status.disgust, status.sad, status.scared, status.neutral
+        with open('status.json', 'r') as f:
+            status = json.load(f)
+
+        return render_template('resultpred.html', prediction=status['happy'], prediction1=status['angry'], prediction2=status['surprised'], prediction3=status['disgust'], prediction4=status['sad'], prediction5=status['scared'], prediction6=status['neutral'])
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
